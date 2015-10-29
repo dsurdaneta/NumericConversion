599f4f9595ca80068a98ee12737c8d60a07babe1
Daniel Urdaneta <mangafever@gmail.com>
2015-10-28 20:21:38 -0430

    Numeric Conversion Library. To binary, to Octal, and to Hexadecimal

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int baseTwo = NumericConversion.Transform.ToBinaryNumber(n);
+            int baseEight = NumericConversion.Transform.ToOctalNumber(n);
+            string base16 = NumericConversion.Transform.ToHex(n);
+
+            Console.WriteLine("Binary conversion: " + baseTwo);
+            Console.WriteLine("Octal conversion: " + baseEight);
+            Console.WriteLine("Hexadecimal conversion: " + base16);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NumericConversion/Transform.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericConversion
+{
+    public class Transform
+    {
+        protected static string _error;
+
+        public string ValidationMessage
+        {
+            get { return _error; }
+        }
+
+        public Transform()
+        {
+            _error = "";
+        }
+
+        public static int ToBinaryNumber(int number)
+        {
+            return Convert.ToInt32(Convert.ToString(number, 2));;
+        }
+
+        public static int ToOctalNumber(int number)
+        {
+            return Convert.ToInt32(Convert.ToString(number, 8));
+        }
+
+        public static string ToHex(int number)
+        {
+            return Convert.ToString(number, 16).ToUpper();
+        }
+    }
+}
