eb3a954789b427ea32a1897faa67b87a5615ab7d
Daniel Urdaneta <dsurdaneta@gmail.com>
2017-05-29 09:36:21 +0200

    Reverse Conversions
    var and consts change to better reading
    methods added: IntegerToOctalString, BinaryLongToInt, BinaryStringToInt, OctalStringToInt, HexStringToInt,

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -17,7 +17,7 @@ namespace ConsoleApplication1
             Console.WriteLine($"Binary conversion: {baseTwo}");
             Console.WriteLine($"Octal conversion: {baseEight}");
             Console.WriteLine($"Hexadecimal conversion: {base16}");
-
+            
             Console.ReadKey();
         }
     }
--- NumericConversion/Transform.cs
+++ NumericConversion/Transform.cs
@@ -6,7 +6,8 @@ namespace NumericConversion
     public class Transform
     {
         protected static string _error;
-        internal const string L = "1000000";
+        internal const string BinaryStringMaxLength = "1000000";
+        internal const string HexPrefix = "0x";
 
         public string ValidationMessage
         {
@@ -20,15 +21,15 @@ namespace NumericConversion
 
         public static long IntegerToBinaryLong(int number)
         {
-            long n = -1;
-            string s = Convert.ToString(number, 2);
-            if(s.Length <= L.Length)
-                n = Convert.ToInt64(s);
+            long binaryLong = -1;
+            string binaryString = Convert.ToString(number, 2);
+            if(binaryString.Length <= BinaryStringMaxLength.Length)
+                binaryLong = Convert.ToInt64(binaryString);
             else
                 _error = $"number {number} length not supported";
                 //_error = "number " + number + " length not supported";
 
-            return n;
+            return binaryLong;
         }
 
         public static string IntegerToBinaryString(int number)
@@ -38,22 +39,72 @@ namespace NumericConversion
 
         public static long IntegerToOctalLong(int number)
         {
-            long n = -1;
-            string s = Convert.ToString(number, 2);
-            if (s.Length <= L.Length)
-                n = Convert.ToInt64(Convert.ToString(number, 8));
+            long octalLong = -1;
+            string binaryString = Convert.ToString(number, 2);
+            if (binaryString.Length <= BinaryStringMaxLength.Length)
+                octalLong = Convert.ToInt64(Convert.ToString(number, 8));
             else
                 _error = $"number {number} length not supported";
                  //_error = "number " + number + " length not supported";
 
-            return n;
+            return octalLong;
         }
 
-        public static string IntegerToHexString(int number)
+        public static string IntegerToOctalString(int number)
         {
+            return Convert.ToString(number, 8);
+        }
+
+        /// <summary>
+        /// As most hex literals are handled with the 0x prefix, you can choose if you want 
+        /// the result string formatted with or without the prefix
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="prefixed"></param>
+        /// <returns></returns>
+        public static string IntegerToHexString(int number, bool prefixed = true)
+        {
+            string hexNumber = prefixed ? HexPrefix : "";
+
             //return Convert.ToString(number, 16).ToUpper();
             //ToString("X") already transform to hex
-            return number.ToString("X");
+            hexNumber = hexNumber + number.ToString("X").ToUpper();
+
+            return hexNumber;
+        }
+
+        public static int BinaryLongToInt(long binaryNumber)
+        {
+            int binaryInt = -1;
+            string binaryString = Convert.ToString(binaryNumber);
+            binaryInt = Convert.ToInt32(binaryString, 2);
+            return binaryInt;
+        }
+
+        public static int BinaryStringToInt(string binaryNumber)
+        {
+            return Convert.ToInt32(binaryNumber, 2);
+        }
+
+        public static int OctalStringToInt(string octalNumber)
+        {
+            return Convert.ToInt32(octalNumber, 8);
+        }
+
+        public static int HexStringToInt(string hexNumber)
+        {
+            int resultValue = 0;
+
+            if (hexNumber.StartsWith(HexPrefix))
+            {
+                resultValue = Convert.ToInt32(hexNumber, 16);
+            }
+            else
+            {
+                resultValue = int.Parse(hexNumber, System.Globalization.NumberStyles.HexNumber);
+            }
+
+            return resultValue;
         }
     }
 }
