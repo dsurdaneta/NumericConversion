513e142eb3cee50e18f01843799fd6c97dd0e649
Daniel Urdaneta <dsurdaneta@gmail.com>
2017-06-01 14:59:58 +0200

    Some basic test added

--- NumericConversionTest/TransformTest.cs
+++ NumericConversionTest/TransformTest.cs
@@ -8,8 +8,24 @@ namespace NumericConversionTest
     public class TransformTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IntToBinaryLongTest()
         {
+            long baseTwo = Transform.IntegerToBinaryLong(3);
+            Assert.AreEqual(baseTwo,11);
+        }
+
+        [TestMethod]
+        public void IntToOctalLongTest()
+        {
+            long baseEight = Transform.IntegerToOctalLong(8);
+            Assert.AreEqual(baseEight,10);
+        }
+
+        [TestMethod]
+        public void HexStringTypeTest()
+        {
+            var hexString = Transform.IntegerToHexString(25);
+            Assert.IsInstanceOfType(hexString,typeof(string));
         }
     }
 }
