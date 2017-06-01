df21bdfc4784da53cf929d2bc54a003b07e4af7c
Daniel Urdaneta <dsurdaneta@gmail.com>
2017-06-01 13:05:05 +0200

    Test Project Add

---
+++ NumericConversionTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NumericConversionTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("NumericConversionTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef7523fb-5264-4883-a224-7193644d9c5b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NumericConversionTest/TransformTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumericConversion;
+
+namespace NumericConversionTest
+{
+    [TestClass]
+    public class TransformTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
