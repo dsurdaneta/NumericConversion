8eb31a48d3d73686697d053771d7354db3aa89ba
Daniel Urdaneta <dsurdaneta@gmail.com>
2017-06-08 16:50:39 +0200

    Roman Numerals added

---
+++ NumericConversion/RomanNumeral.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsuDev.NumericConversion
+{
+    public class RomanNumeral
+    {
+        protected readonly Dictionary<int, string> baseNumbers;
+        protected const int MaxNumber = 4000;
+
+        public RomanNumeral()
+        {
+            baseNumbers = new Dictionary<int, string>();
+            baseNumbers.Add(1, "I");
+            baseNumbers.Add(4, "IV");
+            baseNumbers.Add(5, "V");
+            baseNumbers.Add(9, "IX");
+            baseNumbers.Add(10, "X");
+            baseNumbers.Add(40, "XL");
+            baseNumbers.Add(50, "L");
+            baseNumbers.Add(90, "XC");
+            baseNumbers.Add(100, "C");
+            baseNumbers.Add(400, "CD");
+            baseNumbers.Add(500, "D");
+            baseNumbers.Add(900, "CM");
+            baseNumbers.Add(1000, "M");
+        }
+
+        public string GetRomanValueFromArabicNum(int number)
+        {
+            if (number < 1 || number >= MaxNumber)
+            {
+                throw new ArgumentOutOfRangeException($"{number} must be a positive integer of value less than {MaxNumber}");
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public int GetArabicFromRoman(string romanNumeral)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NumericConversionTest/RomanTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DsuDev.NumericConversion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumericConversion.Test
+{
+    [TestClass]
+    public class RomanTest
+    {
+        [TestMethod]
+        public void RomanNumeral_IsNotNull()
+        {
+            // Arrange
+            var romanNumeral = new RomanNumeral();
+
+            // Act
+            // Assert
+            Assert.IsNotNull(romanNumeral);
+        }
+         
+        [TestMethod]
+        public void RomanNumeral_IsString()
+        {
+            // Arrange
+            var romanNumeral = new RomanNumeral();
+
+            // Act
+            // Assert
+            Assert.IsInstanceOfType(romanNumeral.GetRomanValueFromArabicNum(10), typeof(string));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void RomanNumeral_MaxRange()
+        {
+            // Arrange
+            var romanNumeral = new RomanNumeral();
+
+            // Act
+            romanNumeral.GetRomanValueFromArabicNum(5000);
+            // Assert
+        }
+    }
+}
