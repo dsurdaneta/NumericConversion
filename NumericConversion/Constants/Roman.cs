7663744a4e347dd02096bc2f3c56d2fc16799c72
Daniel Urdaneta <daniel.urdaneta@contractors.roche.com>
2019-12-10 14:47:56 +0100

    moved summary to class level

--- NumericConversion/Constants/Roman.cs
+++ NumericConversion/Constants/Roman.cs
@@ -1,10 +1,10 @@
 ﻿namespace DsuDev.NumericConversion.Constants
 {
-	public class Roman
+    /// <summary>
+    /// Maxim known Roman Numeral is 3999 = "MMMCMXCIX"
+    /// </summary>
+    public class Roman
 	{
-		/// <summary>
-		/// Maxim known Roman Numeral is 3999 = "MMMCMXCIX"
-		/// </summary>
 		internal const int MaxNumber = 4000;
 		public const int MaxAllowedNumber = 3999;
 		public const string MaxAllowedNumeral = "MMMCMXCIX";
